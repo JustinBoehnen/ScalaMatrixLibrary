ea6599635816f59d019cd9a804adfca8c087cb1a
mengistristen <tristen.mengis@gmail.com>
2019-05-16 10:50:31 -0700

    Removed local variable _matrix from ResultFrame.java

--- src/main/scala/gui_calculator/ResultFrame.java
+++ src/main/scala/gui_calculator/ResultFrame.java
@@ -7,15 +7,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ResultFrame extends JFrame {
-    private Matrix _matrix;
-
     public ResultFrame(Matrix matrix, String opType) {
         super("Result: " + opType);
-        _matrix = matrix;
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setSize(new Dimension(400, 400));
         MatrixPanelWithLabels resPan = new MatrixPanelWithLabels();
-        resPan.setMatrix(_matrix);
+        resPan.setMatrix(matrix);
         this.add(resPan);
         this.setVisible(true);
     }
